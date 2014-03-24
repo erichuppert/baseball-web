5ce8d9626e6f1eaf1264f86d6bd457ed62459530
eric <eric@erichuppert.com>
2014-03-24 15:47:46 -0700

    simple funcitonality

--- controllers/matchup.js
+++ controllers/matchup.js
@@ -1,7 +1,7 @@
 function MatchupController($scope, $rootScope, $location, $http) {
 	var pitcherID = $location.search().p;
 	var batterID = $location.search().b;
-	console.log('data');
+	$scope.foo = 'bar';
 	// get matchups
 	$http({
 		url: $rootScope.apiURL + "/atbats",
@@ -16,37 +16,28 @@ function MatchupController($scope, $rootScope, $location, $http) {
 	}).error(function(data, status){
 		$scope.data = data || "Request Failed";
 		$scope.status = status;
-	})
-
+	});
 	// get pitcher
 	$http({
-		url: $rootScope.apiURL + "/players",
+		url: $rootScope.apiURL + "/players/" + pitcherID,
 		method: "GET",
-		params: {
-			id: pitcherID
-		}
 	}).success(function(data, status){
 		$scope.status = status;
 		$scope.pitcher = data;
-		console.log('data');
 	}).error(function(data, status){
 		$scope.data = data || "Request Failed";
 		$scope.status = status;
-		console.log('data');	
-	})
+	});
 
 	// get batter
 	$http({
-		url: $rootScope.apiURL + "/players",
+		url: $rootScope.apiURL + "/players/" + batterID,
 		method: "GET",
-		params: {
-			id: batterID
-		}
 	}).success(function(data, status){
 		$scope.status = status;
 		$scope.batter = data;
 	}).error(function(data, status){
 		$scope.data = data || "Request Failed";
 		$scope.status = status;
-	})	
+	});
 }
